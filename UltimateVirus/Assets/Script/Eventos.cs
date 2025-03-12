96d7863dd980b87b80f58ed7ec256057a5a24939
VictorLopesD <46869529+VictorLopesD@users.noreply.github.com>
2025-03-11 22:03:55 -0300

    atributos

---
+++ UltimateVirus/Assets/Script/Eventos.cs
@@ -0,0 +1,37 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Eventos : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Text txtEvent;
+    [SerializeField]
+    int numEventAleat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EventAleat()
+    {
+        switch(numEventAleat)
+        {
+            case 0:
+
+                break;
+
+        }
+
+    }
+}
