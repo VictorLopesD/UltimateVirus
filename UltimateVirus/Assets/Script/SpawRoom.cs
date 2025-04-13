989bbb093bfe031449ff1d9ab80bc5fc913d43b2
VictorLopesD <46869529+VictorLopesD@users.noreply.github.com>
2025-04-13 14:08:18 -0300

    coliider in rooms

---
+++ UltimateVirus/Assets/Script/SpawRoom.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawRoom : MonoBehaviour
+{
+
+    public int openingDirection;
+    string spawName ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawName = this.gameObject.name;
+        
+        switch (spawName)
+        {
+            case "spawT":
+                openingDirection = 1;
+                break;
+            case "spawR":
+                openingDirection = 2;
+                break;
+            case "spawD":
+                openingDirection = 3;
+                break;
+            case "spawL":
+                openingDirection = 4;
+                break;
+            default:
+                openingDirection = 0;
+                break;
+
+
+        }
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
